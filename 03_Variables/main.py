4fcd498827df5e6e8c3f7337ae7be4137f1c7e22
vidurpuri <vidur0610.puri@gmail.com>
2025-07-03 16:59:16 +0530

    Variables

---
+++ 03_Variables/main.py
@@ -0,0 +1,17 @@
+age = 31 #integer
+print(age)
+print(type(age)) #prints the type of the variable
+
+name = "Vidur" #string
+print(name)
+print(type(name)) #prints the type of the variable
+
+height = 5.9 #float
+print(height)
+print(type(height)) #prints the type of the variable
+
+is_student = True #boolean
+print(is_student)
+print(type(is_student)) #prints the type of the variable
+
+#34age = 31 #invalid variable name, cannot start with a number
