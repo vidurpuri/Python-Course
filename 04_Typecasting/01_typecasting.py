a712f8ec5ba2069603cba249f5a98a9897f1442a
vidurpuri <vidur0610.puri@gmail.com>
2025-07-03 17:08:23 +0530

    Typecasting

---
+++ 04_Typecasting/01_typecasting.py
@@ -0,0 +1,18 @@
+a = 31
+b = "31"
+
+#typecast b to an integer
+c = int(b)  
+print(c)
+print(type(c))  # prints the type of the variable c
+
+# typecast a to string
+d = str(a)
+print(d)
+print(type(d))
+
+int("31")  # typecast string to integer
+float("31.5")  # typecast string to float   
+bool(1)  # typecast integer to boolean
+bool(0)  # typecast integer to boolean
+str() # typecast to string
